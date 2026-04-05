1084915c39f53fad7a8369e78f9e26b9c24ad090
kitzsh <110337474+kitzsh@users.noreply.github.com>
2026-04-05 23:33:57 +1000

    Update webpack.config.js and npm scripts to allow building

--- webpack.config.js
+++ webpack.config.js
@@ -1,101 +1,103 @@
-const path = require('path');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const argv = require('optimist').argv;
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-let entry = [
-    'babel-polyfill',
-    './src/client/index'
-];
+export default (env) => {
+    let entry = [
+        'babel-polyfill',
+        './src/client/index'
+    ];
 
-let plugins = [];
+    let plugins = [];
 
-let devtool = 'eval-source-map';
-let output = 'static/js/index.js';
-let debug = true;
+    let devtool = 'eval-source-map';
+    let output = 'static/js/index.js';
+    
+    let PLATFORM = env.platform || 'web';
+    let NODE_ENV = env.build ? 'production' : 'development';
 
-let PLATFORM = argv.platform || 'web';
-let NODE_ENV = argv.build ? 'production' : 'development';
+    let target = 'web';
+    if (PLATFORM === 'electron') target = 'electron-renderer';
 
-let target = 'web';
-if (PLATFORM === 'electron') target = 'electron-renderer';
-
-plugins.push(new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-    'PLATFORM': JSON.stringify(PLATFORM)
-}));
-
-if (argv.build) {
-    let outputDir;
-
-    if (PLATFORM === 'web') {
-        outputDir = 'web/';
-    }
-
-    if (PLATFORM === 'electron') {
-        outputDir = '../electron/www/';
-    }
-
-    plugins.push(new CopyWebpackPlugin({
-        patterns: [{from: 'src/client/resources', to: outputDir}]
+    plugins.push(new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+        'PLATFORM': JSON.stringify(PLATFORM)
     }));
 
-    devtool = false;
-    output = outputDir + 'static/js/index.js';
-    debug = false;
-}
-else {
-    entry.push('webpack-dev-server/client?http://localhost:4000');
-    plugins.push(new CopyWebpackPlugin({
-        patterns: [{from: 'src/client/resources', to: './'}]
-    }));
-}
+    if (env.build) {
+        let outputDir;
 
-let config = {
-    entry: entry,
-    output: {
-        path: __dirname + "/dist",
-        filename: output
-    },
-    devtool: devtool,
-    target: target,
-    mode: NODE_ENV,
-    module: {
-        noParse: /.*[\/\\]bin[\/\\].+\.js/,
-        rules: [
-            {
-                test: /.jsx?$/,
-                include: [path.resolve(__dirname, 'src')],
-                use: [{loader: 'babel-loader', options: {presets: ['@babel/preset-react', '@babel/preset-env']}}]
-            },
-            {
-                test: /\.js$/,
-                include: [path.resolve(__dirname, 'src')],
-                use: [{loader: 'babel-loader', options: {presets: ['@babel/preset-env']}}]
-            },
-            {
-                test: /\.(html|htm)$/,
-                use: [{loader: 'dom'}]
-            }
-        ]
-    },
-    optimization: {
-        minimize: false
-    },
-    plugins: plugins
-};
+        if (PLATFORM === 'web') {
+            outputDir = 'web/';
+        }
 
-if (target === 'electron-renderer') {
-    config.resolve = {alias: {'platform': path.resolve(__dirname, './src/client/platform/electron')}};
-} else {
-    config.resolve = {alias: {'platform': path.resolve(__dirname, './src/client/platform/web')}};
+        if (PLATFORM === 'electron') {
+            outputDir = '../electron/www/';
+        }
+
+        plugins.push(new CopyWebpackPlugin({
+            patterns: [{from: 'src/client/resources', to: outputDir}]
+        }));
+
+        devtool = false;
+        output = outputDir + 'static/js/index.js';
+    }
+    else {
+        entry.push('webpack-dev-server/client?http://localhost:4000');
+        plugins.push(new CopyWebpackPlugin({
+            patterns: [{from: 'src/client/resources', to: './'}]
+        }));
+    }
+
+    let config = {
+        entry: entry,
+        output: {
+            path: __dirname + "/dist",
+            filename: output
+        },
+        devtool: devtool,
+        target: target,
+        mode: NODE_ENV,
+        module: {
+            noParse: /.*[\/\\]bin[\/\\].+\.js/,
+            rules: [
+                {
+                    test: /.jsx?$/,
+                    include: [path.resolve(__dirname, 'src')],
+                    use: [{loader: 'babel-loader', options: {presets: ['@babel/preset-react', '@babel/preset-env']}}]
+                },
+                {
+                    test: /\.js$/,
+                    include: [path.resolve(__dirname, 'src')],
+                    use: [{loader: 'babel-loader', options: {presets: ['@babel/preset-env']}}]
+                },
+                {
+                    test: /\.(html|htm)$/,
+                    use: [{loader: 'dom'}]
+                }
+            ]
+        },
+        optimization: {
+            minimize: false
+        },
+        plugins: plugins
+    };
+
+    if (target === 'electron-renderer') {
+        config.resolve = {alias: {'platform': path.resolve(__dirname, './src/client/platform/electron')}};
+    } else {
+        config.resolve = {alias: {'platform': path.resolve(__dirname, './src/client/platform/web')}};
+    }
+
+    config.resolve.fallback = {
+        "stream": import.meta.resolve("stream-browserify"),
+        "timers": import.meta.resolve("timers-browserify"),
+        "buffer": import.meta.resolve("buffer/")
+    }
+
+    return config;
 }
-
-config.resolve.fallback = {
-    "stream": require.resolve("stream-browserify"),
-    "timers": require.resolve("timers-browserify"),
-    "buffer": require.resolve("buffer/")
-}
-
-module.exports = config;
